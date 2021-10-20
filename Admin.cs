a5a0f4faad587d3e8b5912f425f3494523c39680
Chloe Stephan <chloe.stephan@efrei.net>
2021-10-20 14:12:07 +0200

    Ajoutez des fichiers projet.

---
+++ Admin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace ATM
+{
+    class Admin
+    {
+        /*
+        private void ShowUserDetails()
+        {
+            Console.Clear();
+            Center("**** GTBPI Banking System | Account Details ****\n");
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            DrawLine();
+            Console.WriteLine("|{0}|",AlignText(0, ""));
+            Console.WriteLine("|{0}|",AlignText(25, "Account Number            :  " + User.Account_Number));
+            Console.WriteLine("|{0}|",AlignText(25, "Account Holder's Name     :  " + User.Title + ". " + User.Name));
+            Console.WriteLine("|{0}|",AlignText(25, "Total Balance in account  :  " + "Rs. " + User.Total_Balance));
+            Console.WriteLine("|{0}|",AlignText(25, "Last Login Details        :  " + User.LastLoginDetails));
+            Console.WriteLine("|{0}|",AlignText(0, ""));
+            DrawLine();
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine("\n");
+            Center("Press any key to go back to the previous menu!");
+        }
+        */
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Data.SQLite;
+
+namespace ATM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Database databaseObject = new Database();
+
+
+            /**
+             * Insert data into the database Clients - for each new person, manually change the values in the AddWithValue lines
+             * */
+
+            /*
+            string query = "INSERT INTO clients ('GUID', 'FirstName', 'LastName', 'PIN', 'MainCurrency') VALUES (@GUID, @FirstName, @LastName, @PIN, @MainCurrency)";
+
+            SQLiteCommand myCommand = new SQLiteCommand(query, databaseObject.myConnection);
+
+            databaseObject.OpenConnection();
+            
+            myCommand.Parameters.AddWithValue("@GUID", "4E21955A-7707-4F65-BD67-7341CC6A5051");
+            myCommand.Parameters.AddWithValue("@FirstName", "Quentin");
+            myCommand.Parameters.AddWithValue("@LastName", "Mourey");
+            myCommand.Parameters.AddWithValue("@PIN", "9101");
+            myCommand.Parameters.AddWithValue("@MainCurrency", "Euro");
+            var result = myCommand.ExecuteNonQuery();
+
+            databaseObject.CloseConnection();
+
+            //check that the value has been correctly added :
+            //Console.WriteLine("Rows added: {0}", result);
+
+            */            
+
+           /**
+             * Select data into the database Clients - returns everything
+             * */
+
+            string query = "SELECT * FROM clients";
+
+            SQLiteCommand myCommand = new SQLiteCommand(query, databaseObject.myConnection);
+
+            databaseObject.OpenConnection();
+
+            SQLiteDataReader result = myCommand.ExecuteReader();
+
+            if (result.HasRows)
+            {
+                while (result.Read())
+                {
+                    Console.WriteLine("GUID: {0} - First Name: {1} - Last Name: {2} - PIN: {3} - Main Currency: {4}", result["GUID"], result["FirstName"], result["LastName"], result["PIN"], result["MainCurrency"]);
+                }
+            }
+
+            databaseObject.CloseConnection();
+
+
+            Console.ReadKey();
+        }
+    }
+}
